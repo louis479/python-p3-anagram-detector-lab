512b3f7bb61cca421bbfb8a6064a959b2d5f9623
louis-ngugi <louisngugi858@gmail.com>
2025-02-21 12:43:13 +0300

    updated anagram.py & debug.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+from collections import Counter
+
+class Anagram :
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        word_counter = Counter(self.word)
+        return [word for word in word_list if Counter(word) == word_counter]
+
+listen = Anagram("listen")
+print(listen.match(['enlist', 'google', 'inlets', 'banana']))
--- lib/debug.py
+++ lib/debug.py
@@ -4,3 +4,4 @@ from anagram import Anagram
 
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
+
